1057d70f1b86721d84d2379021f0b64a47e7eb72
ktrauma <124712839+ktrauma@users.noreply.github.com>
2023-03-22 11:09:57 -0700

    updated current temperature in js

--- Vanilla Weather App/src/app.js
+++ Vanilla Weather App/src/app.js
@@ -1,1 +1,20 @@
+function displayTemp(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let precipitationElement = document.querySelector("#precipitation");
+  precipitationElement.innerHTML = Math.round(response.data.main.feels_like);
+}
 
+let apiKey = "2718952144ed077c12e7c160fb6fc351";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemp);
